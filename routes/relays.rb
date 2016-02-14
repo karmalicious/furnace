168dcfa9161bd11f7e2e9bcb4792b0e1b445c2ef
Michael Norrving <michael@norrving.se>
2016-02-14 11:42:38 +0100

    Rooms now get and update correctly with data the client sends

--- routes/relays.rb
+++ routes/relays.rb
@@ -1,7 +1,8 @@
 post '/api/relay' do
   body = JSON.parse request.body.read
-  relay = Unit.first(:unit => body['unit']).rooms.get(:room => body['room']).roomdata.update(
+  relay = Unit.first(:unit => body['unit']).rooms.first(:room => body['room']).update(
     :relay_status       => body['relay_status'],
+    :temp		=> body['temp'],
     :updated_at         => DateTime.now
   )
   status 201
